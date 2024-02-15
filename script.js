568b8a37fde18504c65724372aa5d0eafe70a75b
kxtmg <disizkatie@gmail.com>
2024-02-14 18:04:36 -0800

    Organized JavaScript
    cleaned up the javascript and included necessary images and files

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function clickedYes(element) {
+const buttonText = document.querySelector("#no");
+const prompts = [
+ "please", 
+ "i'm begging", 
+ "please reconsider", 
+ "are you sure", 
+ "grant me another chance", 
+ "give me an opportunity", 
+ "can you be absolutely certain about this?", 
+ "I implore you with every fiber of my being, please, I'm on my knees!"];
+
+const question = document.getElementById("question");
+const button1 = document.getElementById("yes");
+const button2 = document.getElementById("no");
+
+let count = 0;
+
+function clickedYes() {
     // "yippie!" w gif and sound
     document.getElementById("gif").src="yippie.gif";
-
-    const question = document.getElementById("question");
-    const button1 = document.getElementById("yes");
-    const button2 = document.getElementById("no");
 
     question.remove();
     button1.remove();
@@ -15,23 +28,12 @@ function clickedYes(element) {
 }
 
 function clickedNo() {
-    const buttonText = document.querySelector("#no");
-    const prompts = ["null", "please", "i'm begging", "please reconsider", "are you sure", 
-    "grant me another chance", "give me an opportunity", 
-    "can you be absolutely certain about this?", 
-    "I implore you with every fiber of my being, please, I'm on my knees!"];
-    
-    let count = 0;
-    let btn = document.getElementById("no");
-    const button1 = document.getElementById("yes");
-    btn.onclick = function(){
-        count++;
-        if (count < prompts.length) {
-            buttonText.innerHTML = prompts[count];
-        } else {
-            buttonText.innerHTML = "damn fr?";
-            document.getElementById("gif").src="disappointed.png";
-            button1.remove();
-        }
+    if (count < prompts.length) {
+        buttonText.innerHTML = prompts[count];
+    } else {
+        buttonText.innerHTML = "damn fr?";
+        document.getElementById("gif").src="disappointed.png";
+        button1.remove();
     }
+    count++;
 }
